c1e54a832c57703fefce345dc35141e030afbddd
amenallahg122 <bigpapiamen@gmail.com>
2025-03-04 02:51:41 +0100

    a lot of coins

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,23 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public float speed = 5f;
+    private int score = 0;
+    private Rigidbody rb;
 
-	// Use this for initialization
-	public float speed = 5f;
-	private Rigidbody rb;
-	
-	void Start () {
-		rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate()
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
     {
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveX, 0, moveZ) * speed;
+        Vector3 movement = new Vector3(moveX, 0, moveZ) * speed * Time.deltaTime;
 
-        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+        rb.velocity = new Vector3(0, rb.velocity.y, 0);
+
+        rb.MovePosition(transform.position + movement);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            score++;
+            Debug.Log("Score: " + score);
+            other.gameObject.SetActive(false);
+        }
     }
 }
