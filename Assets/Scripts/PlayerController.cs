a6730b8a3333b8f0ff9dbf5b77176987e442b536
amenallahg122 <bigpapiamen@gmail.com>
2025-03-04 03:21:33 +0100

    Game over

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class PlayerController : MonoBehaviour {
     public float speed = 5f;
@@ -43,6 +45,22 @@ public class PlayerController : MonoBehaviour {
             {
                 Debug.Log("Game Over!");
             }
+        }
+		if (other.CompareTag("Goal"))
+        {
+            Debug.Log("You win!");
+        }
+    }
+	    void Update()
+    {
+        if (health <= 0)
+        {
+            Debug.Log("Game Over!");
+
+            score = 0;
+            health = 5;
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
