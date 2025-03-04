aa0bf87bd52f08c28352c6d82df7031025d66e1d
amenallahg122 <bigpapiamen@gmail.com>
2025-03-04 02:12:54 +0100

    movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed = 5f;
+	private Rigidbody rb;
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0, moveZ) * speed;
+
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+    }
+}
